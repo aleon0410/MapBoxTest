59c91d6bd11d4a5668082c6fd73bcfa4f3a3999e
Andrés León <andresleon83@gmail.com>
2015-09-18 06:26:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/leones/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.leones.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.GpsLocationProvider;
+import com.mapbox.mapboxsdk.overlay.Marker;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private GpsLocationProvider locationProvider;
+    private LatLng myCoords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapView = (MapView)findViewById(R.id.mapview);
+
+        locationProvider = new GpsLocationProvider(getApplicationContext());
+
+        UserLocationOverlay myLocationOverlay = new UserLocationOverlay(locationProvider, mapView);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.setDrawAccuracyEnabled(true);
+        mapView.getOverlays().add(myLocationOverlay);
+
+        myCoords = new LatLng(locationProvider.getLastKnownLocation().getLatitude(), locationProvider.getLastKnownLocation().getLongitude());
+
+        mapView.setCenter(myCoords);
+        mapView.setZoom(12);
+
+        Marker marker = new Marker(mapView,"Prueba","Prueba",myCoords);
+        mapView.addMarker(marker);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
